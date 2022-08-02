ac952bfcd52f41c2e03a7de398786cc25b840748
Katerina Shchablovskaya <shablovskaykate@mail.ru>
2022-08-02 15:43:52 +0300

    fix:change slider 391-1439 screen resolution

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ window.addEventListener("resize", function () {
     burger_menu.classList.remove("active");
     burger_line.classList.remove("active");
   }
+  if (window.matchMedia("(max-width:390px)").matches) {
+    sliderFor390px();
+    console.log("test")
+  }
 });
 //const body = document.querySelector("body");
 burger_menu_button.addEventListener("click", tooggleMenu);
@@ -25,69 +29,71 @@ menu_btn.forEach((el) => {
 
     });
 });*/
+  const sliderLine = document.querySelector(".slider-line");
+  const indicators = document.querySelectorAll(".indicators");
+  const arrowLeft = document.querySelector(".arrow-left");
+  const arrowRight = document.querySelector(".arrow-right");
+  const arrowBut = document.querySelectorAll(".arrow");
 
-//Slider - 390
-let offset = 0;
-let index = 0;
-let widthSlide = 360;
-const sliderLine = document.querySelector(".slider-line");
-const indicators = document.querySelectorAll(".indicators");
-const arrowLeft = document.querySelector(".arrow-left");
-const arrowRight = document.querySelector(".arrow-right");
-const arrowBut = document.querySelectorAll(".arrow");
+const sliderFor390px = function () {
+  //Slider - 390
+  let offset = 0;
+  let widthSlide = 360;
+  
+  const activeIndicators = function (index) {
+    for (ind of indicators) {
+      ind.classList.remove("indicators_selected");
+    }
+    indicators[index].classList.add("indicators_selected");
+    console.log(index);
+  };
 
-const activeIndicators = function (index) {
-  for (ind of indicators) {
-    ind.classList.remove("indicators_selected");
-  }
-  indicators[index].classList.add("indicators_selected");
-  console.log(index);
-};
-
-arrowRight.addEventListener("click", function (event) {
-  event.stopPropagation();
-  offset += widthSlide;
-  if (offset >= widthSlide * 3) {
-    offset = 0;
-  }
-  moveToSlide(offset);
-  activeIndicators(offset / widthSlide);
-  disabledArrow(offset);
-  console.log(offset);
-});
-const moveToSlide = function (num) {
-  sliderLine.style.left = -num + "px";
-};
-
-const disabledArrow = function (num) {
-  for (button of arrowBut) {
-    button.classList.remove("disabled");
-  }
-  if (num === 0) {
-    arrowLeft.classList.add("disabled");
-  }
-  if (Math.abs(num) === 720) {
-    arrowRight.classList.add("disabled");
-  }
-};
-
-arrowLeft.addEventListener("click", function (event) {
-  event.stopPropagation();
-  offset -= widthSlide;
-  if (offset < 0) {
-    offset = widthSlide * 2;
-  }
-  moveToSlide(offset);
-  activeIndicators(Math.abs(offset / widthSlide));
-  disabledArrow(offset);
-  console.log(offset);
-});
-
-indicators.forEach((item, indexInd) => {
-  item.addEventListener("click", () => {
-    offset = indexInd * widthSlide;
-    activeIndicators(indexInd);
+  arrowRight.addEventListener("click", function (event) {
+    event.stopPropagation();
+    offset += widthSlide;
+    if (offset >= widthSlide * 3) {
+      offset = 0;
+    }
     moveToSlide(offset);
+    activeIndicators(offset / widthSlide);
     disabledArrow(offset);
+    console.log(offset);
   });
-});
+  const moveToSlide = function (num) {
+    sliderLine.style.left = -num + "px";
+  };
+
+  const disabledArrow = function (num) {
+    for (button of arrowBut) {
+      button.classList.remove("disabled");
+    }
+    if (num === 0) {
+      arrowLeft.classList.add("disabled");
+    }
+    if (Math.abs(num) === widthSlide * 2) {
+      arrowRight.classList.add("disabled");
+    }
+  };
+
+  arrowLeft.addEventListener("click", function (event) {
+    event.stopPropagation();
+    offset -= widthSlide;
+    if (offset < 0) {
+      offset = widthSlide * 2;
+    }
+    moveToSlide(offset);
+    activeIndicators(Math.abs(offset / widthSlide));
+    disabledArrow(offset);
+    console.log(offset);
+  });
+
+  indicators.forEach((item, indexInd) => {
+    item.addEventListener("click", () => {
+      offset = indexInd * widthSlide;
+      activeIndicators(indexInd);
+      moveToSlide(offset);
+      disabledArrow(offset);
+    });
+  });
+ 
+};
